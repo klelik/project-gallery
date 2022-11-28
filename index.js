a2c31cba0f3a348ca50ddbf9f356f00c6ce16d12
klelik <klelika1998@gmail.com>
2022-11-28 03:45:40 +0000

    changes on styling

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-$(".btn").on('click',function(){
-  $(this).parent().toggleClass("showContent");
+$("#calculator").click(function () {
+  window.location.href = "https://github.com/klelik/calculator";
+});
+$("#clone").click(function () {
+  window.location.href = "https://github.com/klelik/airbnb-clone";
+});
+$("#drum").click(function () {
+  window.location.href = "https://github.com/klelik/drum-machine";
+});
+$("#quote").click(function () {
+  window.location.href = "https://github.com/klelik/quotte-machine";
 });
